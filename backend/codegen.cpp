3eb0917128be41519e18526f8a2e489541c1f1f2
shalevshagan1 <48890926+shalevshagan1@users.noreply.github.com>
2023-06-26 13:39:05 +0300

    Updated Main

--- backend/codegen.cpp
+++ backend/codegen.cpp
@@ -1,91 +1,121 @@
 #include <vector>
 #include <string>
+#include <memory>
 #include "types.h"
+#include "codegen.h"
+
+MOV::MOV(std::string dest, std::string source) {
+    this->NAME = "MOV";
+    this->params[0] = dest;
+    this->params[1] = source;
+}
+std::string MOV::assemble() {
+    return this->NAME + " " + this->params[0] + " " + this->params[1];
+}
+
+PUSHAD::PUSHAD()
+{
+    this->NAME = "PUSHAD";
+}
+std::string PUSHAD::assemble()
+{
+    return this->NAME;
+}
+
+POPAD::POPAD()
+{
+    this->NAME = "POPAD";
+}
+std::string POPAD::assemble() 
+{
+    return this->NAME;
+}
+
+INVOKE::INKOKE(std::string expression, std::vector<std::string> arguments) 
+{
+    this->NAME = "INVOKE";
+    this->expression = expression;
+    this->params = arguments;
+}
+std::string INVOKE::assemble() 
+{
+    return this->NAME + " " + this->expression + " " ;//+ this->params;
+}
 
 
-class Instruction {
-    std::string NAME;
-    std::vector<std::string> params;
+PROTOTYPE::PROTOTYPE(std::string label)
+{
+    this->label = label;
+}
+std::string PROTOTYPE::assemble() 
+{
+    return this->label + this->NAME ;//+ this->params;
+}
 
-
-    std::string generate_asm() {
-        std::string asm_code = this->NAME;
-
-        for (std::string param : params)
-        {
-            asm_code += " " + param;
-        }
-
-        return asm_code;
-    }
-};
-
-class MOV : Instruction {
-
-};
-
-class PUSH : Instruction {
-
-};
-
-class POP : Instruction {
+void Generator::assemble_print(std::vector<std::unique_ptr<ExprAST>> args) 
+{
+    // add 3 function prototypes to headers
+    this->dataseg += PROTOTYPE("GetStdHandle").assemble();
+    this->dataseg += PROTOTYPE("WriteConsoleA").assemble();
+    this->dataseg += PROTOTYPE("ExitProcess").assemble();
     
-};
+    // add INVOKE calls
+    this->codeseg += INVOKE("GetStdHandle", std::vector<std::string>{"-11"}).assemble();
+    this->codeseg += MOV("consoleOutHandle", "eax").assemble();
+    this->codeseg += MOV("").assemble();
+    this->codeseg += PUSHAD().assemble();
+    this->codeseg += MOV().assemble();
+    this->codeseg += INVOKE("WriteConsoleA", ...);
+    this->codeseg += POPAD().assemble();
+    this->codeseg += INVOKE("ExitProcess", 0);
+}
+void Generator::assemble_proc(std::string name, std::unique_ptr<ExprAST> expr)
+{
+    codeseg += name + " PROC";
+    expr->codegen();
+    codeseg += "END " + name;
+}
+void Generator::assemble_number(int value)
+{
+    this->dataseg += "rand_name DWORD {value}\n";
+}
+void Generator::output_all_asm()
+{
+    return this->processor + this->model + this->headers + this->dataseg + this->codeseg;
+}
 
-class INVOKE : Instruction {
+void PrototypeAST::codegen()
+{
 
-};
+}
 
-class PROTOTYPE : Instruction {
-
-};
-
-
-
-class generator {
-    std::string processor = ""; // default is left to the assembler
-    std::string model = ""; // default is left to the assembler
-    std::string headers = ""; 
-    std::string codeseg;
-    std::string dataseg;
-
-    std::vector<Instruction> Instructions;
-
-    void assemble_print(std::vector<std::unique_ptr<ExprAST>> args) 
-    {
-        // add 3 function prototypes to headers
-        
-        // add INVOKE calls
-    }
-
-    void assemble_proc(std::string name)
-    {
-        codeseg += name + " PROC";
-        for (Instruction i : Instructions)
-        {
-
-        }
-        codeseg += "END " + name;
-    }
-};
-
-generator gen;
 void FunctionAST::codegen() {
 
-    for (auto expr : this->body)
-    {
-        expr->codegen();
-    }
+    // this->proto->codegen();
+    this->assemble_proc("main", std::move(this->body));
+    // return 
+}
 
+void NumberExprAST::codegen()
+{
+    gen.assemble_number(this->value); // return name
 }
 
 void CallExprAST::codegen()
 {
     if (this->callee == "print")
     {
-        gen::assemble_print(this->args); // add print to the assembly, it's an external function
+        gen.assemble_print(this->args); // add print to the assembly, it's an external function
     }
-
-
 }
-// print(123)
+
+int main() 
+{
+    // PrototypeAST p("main", std::vector<std::string>());
+    // FunctionAST f(std::move(p), std::make_unique<CallExprAST>("print", std::make_unique<NumberExprAST>(123)));
+
+    // f->codegen();
+}
+
+// decide how to pack args
+// What type should MOV get?
--- main.cpp
+++ main.cpp
@@ -1,238 +1,379 @@
-// #include <string>
-// #include <vector>
-// #include <memory>
-// #include "llvm/IR/IRBuilder.h"
-// #include "llvm/IR/Verifier.h"
-// #include "llvm/IR/LegacyPassManager.h"
-// #include "llvm/MC/TargetRegistry.h"
-// #include "llvm/Support/FileSystem.h"
-// #include "llvm/Support/Host.h"
-// #include "llvm/Support/TargetSelect.h"
-// #include "llvm/Support/raw_ostream.h"
-// #include "llvm/Target/TargetMachine.h"
-// #include "llvm/Target/TargetOptions.h"
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+using namespace llvm;
 
 class Lexer
 {
 public:
-   static char lastchar;
-   static int numvalue; // Filled in if tok_number
+    static char lastchar;
+    static int numvalue;              // Filled in if tok_number
+    static std::string identifierstr; // filled in if tok_identifier
 
-   enum Token
-   {
-       tok_EOF = -1,
-       tok_function = -2,
-       tok_number = -3,
-       tok_return = -4
-   };
+    enum Token
+    {
+        tok_EOF = -1,
+        tok_function = -2,
+        tok_number = -3,
+        tok_return = -4,
+        tok_identifier = -5
+    };
 
-   int get_token(std::string& content)
-   {
-       while (isspace(Lexer::lastchar))
-       {
-           Lexer::lastchar = getnext(content);
-       }
-       if (isalpha(Lexer::lastchar))
-           return Lexer::is_identifier(content);
+    static int getnext(std::string &s)
+    {
+        if (s.length() > 0)
+        {
+            char c = s.at(0);
+            s = s.substr(1, s.length() - 1);
+            return c;
+        }
+        return EOF;
+    }
 
-       if (isdigit(Lexer::lastchar))
-           return Lexer::is_numeric(content);
-       if (Lexer::lastchar == EOF)
-           return tok_EOF;
+    static int get_token(std::string &content)
+    {
+        Lexer::lastchar = ' ';
 
-       return Lexer::lastchar;
-   }
+        while (isspace(lastchar))
+        {
+            lastchar = getnext(content);
+        }
+
+        if (isalpha(lastchar))
+            return is_identifier(content);
+
+        if (isdigit(lastchar))
+            return is_numeric(content);
+        if (lastchar == EOF)
+            return tok_EOF;
+
+        return lastchar;
+    }
+
+    static int is_identifier(std::string &content)
+    {
+        identifierstr = lastchar;
+        while (isalnum(lastchar))
+        {
+            if (isalnum(content.at(0)))
+            {
+                lastchar = getnext(content);
+                identifierstr += lastchar;
+            }
+            else break;
+        }
+        if (identifierstr == "def")
+        {
+            return tok_function;
+        }
+        return tok_identifier;
+    }
+
+    static int is_numeric(std::string &content)
+    {
+        std::string num(1, lastchar);
+        while (isdigit(lastchar))
+        {
+            if (content.length() > 0 && isalnum(content.at(0)))
+            {
+                lastchar = getnext(content);
+                num += lastchar;
+            }
+            else
+                break;
+        }
+
+        Lexer::numvalue = std::stoi(num); // Support only integers so far
+        return tok_number;
+    }
 };
+std::string Lexer::identifierstr;
+int Lexer::numvalue;
+char Lexer::lastchar;
 
-class PrototypeAST
+static int current_token;
+int getNextToken(std::string &line)
 {
-   std::string name;
-   std::vector<std::string> args;
-
-public:
-   PrototypeAST(const std::string& name, std::vector<std::string> Args)
-   {
-       this->name = name;
-       this->args = std::move(Args);
-   }
-};
-
-class FunctionAST
-{
-   std::unique_ptr<PrototypeAST> proto;
-   Block body; // body is collection of expressions
-
-public:
-   FunctionAST(std::unique_ptr<PrototypeAST> Proto, Block Body)
-   {
-       this->proto = std::move(Proto);
-       for (auto&& expr : Body)
-       {
-           this->body.emplace_back(std::move(expr));
-       }
-   }
-};
+    current_token = Lexer::get_token(line);
+    return current_token;
+}
 
 class ExprAST
 {
 public:
-   virtual ~ExprAST() = 0;
+    virtual Value *codegen() = 0;
 };
-typedef std::vector<std::unique_ptr<ExprAST>> Block;
+typedef std::vector<std::unique_ptr<ExprAST>> ExpressionList;
+
+class PrototypeAST
+{
+    std::string name;
+    std::vector<std::string> args;
+
+public:
+    PrototypeAST(const std::string &name, std::vector<std::string> Args)
+    {
+        this->name = name;
+        this->args = std::move(Args);
+    }
+    Function *codegen();
+};
+
+class FunctionAST
+{
+    std::unique_ptr<PrototypeAST> proto;
+    std::unique_ptr<ExprAST> body; // body is collection of expressions
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
+    {
+        this->proto = std::move(Proto);
+        this->body = std::move(Body);
+        // for (auto &&expr : Body)
+        // {
+        //     this->body = std::move(expr);
+        // }
+    }
+    Function *codegen();
+};
 
 class CallExprAST : public ExprAST
 {
-   std::string callee;
-   Block args;
+    std::string callee;
+    ExpressionList args;
 
 public:
-   CallExprAST(const std::string& callee, Block args)
-   {
-       this->callee = callee;
-       this->args = std::move(args);
-   }
+    CallExprAST(const std::string &callee, ExpressionList args)
+    {
+        this->callee = callee;
+        this->args = std::move(args);
+    }
+    Value *codegen() override;
 };
 
 class NumberExprAST : public ExprAST
 {
-   int value;
-
+    int value;
+   
 public:
-   NumberExprAST(int value)
-   {
-       this->value = value;
-   }
+    NumberExprAST(int value)
+    {
+        this->value = value;
+    }
+    Value* codegen() override;
 };
 
-static std::unique_ptr<PrototypeAST> ParsePrototype(std::string& content)
+static std::unique_ptr<ExprAST> ParsePrimary(std::string &content);
+static std::unique_ptr<PrototypeAST> ParsePrototype(std::string &content)
 {
-   std::string FnName = Lexer::identifierstr;
-   getNextToken(content);
-   std::vector<std::string> ArgNames;
-   while (getNextToken(content) == Lexer::tok_identifier)
-       ArgNames.push_back(Lexer::identifierstr);
-   getNextToken(content);
-   return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+    std::string FnName = Lexer::identifierstr;
+    getNextToken(content);
+    std::vector<std::string> ArgNames;
+    while (getNextToken(content) == Lexer::tok_identifier)
+        ArgNames.push_back(Lexer::identifierstr);
+    getNextToken(content);
+    return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+}
+static std::unique_ptr<FunctionAST> ParseDefinition(std::string &content)
+{
+    getNextToken(content); // eat function keyword.
+    auto Proto = ParsePrototype(content);
+    auto body_block = ParsePrimary(content);
+    return std::make_unique<FunctionAST>(std::move(Proto), std::move(body_block));
 }
 
-static std::unique_ptr<FunctionAST> ParseDefinition(std::string& content)
+static std::unique_ptr<ExprAST> ParseNumberExpr(std::string &line)
 {
-   getNextToken(content); // eat function keyword.
-   auto Proto = ParsePrototype(content);
-   auto body_block = ParseBlock(content);
-   return std::make_unique<FunctionAST>(std::move(Proto), std::move(body_block));
+    auto result = std::make_unique<NumberExprAST>(Lexer::numvalue);
+    getNextToken(line);
+    return std::move(result);
+}
+static std::unique_ptr<ExprAST> ParseIdentifierExpr(std::string &line)
+{
+    // ADD TYPE NAMES!
+    std::string id_name = Lexer::identifierstr;
+    getNextToken(line); // skip function identifier
+    getNextToken(line);
+    std::vector<std::unique_ptr<ExprAST>> args; // function args
+    std::unique_ptr<ExprAST> arg;
+
+    while (true)
+    {
+        arg = ParsePrimary(line);
+        if (arg)
+        {
+            args.push_back(std::move(arg));
+        }
+        else
+        {
+            getNextToken(line);
+            return nullptr;
+        }
+
+        if (current_token == ')')
+            break; // Done parsing args
+        getNextToken(line);
+    }
+
+    getNextToken(line);
+
+    return std::make_unique<CallExprAST>(id_name, std::move(args));
+}
+static std::unique_ptr<ExprAST> ParsePrimary(std::string &content)
+{
+    switch (current_token)
+    {
+    case Lexer::tok_identifier:
+        return ParseIdentifierExpr(content);
+    case Lexer::tok_number:
+        return ParseNumberExpr(content);
+    default:
+        return nullptr;
+    }
 }
 
-static std::unique_ptr<ExprAST> ParsePrimary(std::string& content)
+std::unique_ptr<LLVMContext> TheContext;
+std::unique_ptr<Module> TheModule;
+std::unique_ptr<IRBuilder<>> Builder;
+
+void InitializeModule()
 {
-   switch (current_token)
-   {
-   case Lexer::tok_identifier:
-       return ParseIdentifierExpr(content);
-   case Lexer::tok_number:
-       return ParseNumberExpr(content);
-   case '(':
-       return ParseParenExpr(content);
-   case Lexer::tok_return:
-       return ParseReturn(content);
-   case Lexer::tok_function:
-       return ParseDefinition(content);
-   }
+    TheContext = std::make_unique<LLVMContext>();
+    TheModule = std::make_unique<Module>("MyCompiler", *TheContext);
+    Builder = std::make_unique<IRBuilder<>>(*TheContext);
 }
 
-Function* PrototypeAST::codegen()
+Function *PrototypeAST::codegen()
 {
-   std::vector<Type*> Ints(args.size(), Type::getInt32Ty(*TheContext));
-   FunctionType* FT = FunctionType::get(Type::getInt32Ty(*TheContext), Ints, true);
-   Function* F = Function::Create(FT, Function::ExternalLinkage, this->name, TheModule.get());
-   return F;
+    FunctionType *FT = FunctionType::get(Type::getInt32Ty(*TheContext), true);
+    Function *F = Function::Create(FT, Function::ExternalLinkage, this->name, TheModule.get());
+    return F;
 }
 
-Function* FunctionAST::codegen()
+Function *FunctionAST::codegen()
 {
-   Function* TheFunction = this->proto->codegen();
-   BasicBlock* BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
-   Builder->SetInsertPoint(BB);
+    Function *TheFunction = this->proto->codegen();
+    BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
+    Builder->SetInsertPoint(BB);
+    this->body->codegen();
+    Builder->CreateRet(ConstantInt::get(Type::getInt32Ty(*TheContext), 0, false)); // return 0
+    verifyFunction(*TheFunction);
 
-   for (auto& expr : this->body)
-   {
-       expr->codegen();
-   }
-   verifyFunction(*TheFunction);
-
-   return TheFunction;
+    return TheFunction;
 }
 
-Value* NumberExprAST::codegen()
+Value *NumberExprAST::codegen()
 {
-   return ConstantInt::get(Type::getInt32Ty(*TheContext), this->value, true);
+    return ConstantInt::get(Type::getInt32Ty(*TheContext), this->value, true);
 }
 
-Value* CallExprAST::codegen()
+Value *CallExprAST::codegen()
 {
-   // Look up the name in the global module table.
-   Function* CalleeF = TheModule->getFunction(this->callee);
+    // Look up the name in the global module table.
+    Function *CalleeF = TheModule->getFunction(this->callee);
 
-   std::vector<Value*> ArgsV;
-   for (size_t i = 0; i != args.size(); i++)
-   {
-       ArgsV.push_back(args[i]->codegen());
-       if (!ArgsV.back())
-           return nullptr;
-   }
-   return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+    std::vector<Value*> ArgsV;
+    for (size_t i = 0; i != args.size(); i++)
+    {
+        ArgsV.push_back(args[i]->codegen());
+        if (!ArgsV.back())
+            return nullptr;
+    }
+    return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
 }
 
 void GenerateObject(std::string target)
 {
-   try
-   {
-       InitializeAllTargetInfos();
-       InitializeAllTargets();
-       InitializeAllTargetMCs();
-       InitializeAllAsmParsers();
-       InitializeAllAsmPrinters();
+    try
+    {
+        InitializeAllTargetInfos();
+        InitializeAllTargets();
+        InitializeAllTargetMCs();
+        InitializeAllAsmParsers();
+        InitializeAllAsmPrinters();
 
-       TheModule->setTargetTriple(target);
-       auto Target = TargetRegistry::lookupTarget(target, Error);
-       auto CPU = "generic";
-       auto Features = "";
+        TheModule->setTargetTriple(target);
+        std::string Error;
+        auto Target = TargetRegistry::lookupTarget(target, Error);
+        auto CPU = "generic";
+        auto Features = "";
 
-       TargetOptions opt;
-       auto RM = Optional<Reloc::Model>();
-       auto TargetMachine = Target->createTargetMachine(target, CPU, Features, opt, RM);
+        TargetOptions opt;
+        auto RM = Optional<Reloc::Model>();
+        auto TargetMachine = Target->createTargetMachine(target, CPU, Features, opt, RM);
 
-       TheModule->setDataLayout(TargetMachine->createDataLayout());
-       std::error_code EC;
-       raw_fd_ostream dest("a.o", EC, sys::fs::OF_None);
-       legacy::PassManager pass;
-       auto FileType = CGFT_ObjectFile;
+        TheModule->setDataLayout(TargetMachine->createDataLayout());
+        std::error_code EC;
+        raw_fd_ostream dest("a.o", EC, sys::fs::OF_None);
+        legacy::PassManager pass;
+        auto FileType = CGFT_ObjectFile;
 
-       TargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType);
-   }
-   catch (...)
-   {
-   }
+        TargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType);
+        pass.run(*TheModule);
+        dest.flush();
+    }
+    catch (...)
+    {
+    }
 }
 
-void MainLoop(std::string& content)
+void MainLoop(std::string &content)
 {
-   while (1)
-   {
-       switch (current_token)
-       {
-       case Lexer::tok_EOF:
-           return;
-       case Lexer::tok_function:
-           auto F = ParseDefinition(content);
-           F->codegen();
-           break;
-       }
-   }
+    getNextToken(content);
+    while (1)
+    {
+        switch (current_token)
+        {
+        case Lexer::tok_EOF:
+            return;
+        case Lexer::tok_function:
+            auto F = ParseDefinition(content);
+            auto IR = F->codegen();
+            IR->print(errs());
+            break;
+        }
+    }
 }
 
-int main() {
-   std::string content = "def main()\nprint(123)";
-   MainLoop(content);
-   GenerateObject("x86_64-pc-windows");
+Function* AddExternalFunc(std::string name) {
+    FunctionType* FuncType = FunctionType::get(Type::getVoidTy(*TheContext), {Type::getInt32Ty(*TheContext)}, false); // signature is: void f(int) 
+    return Function::Create(FuncType, Function::ExternalLinkage, name, TheModule.get()); // add function to global module table
 }
+
+// create actual print function
+void CreatePrintFunction()
+{
+    Function* PrintfFunc = AddExternalFunc("printf");
+    Function* PrintFunc = AddExternalFunc("print");
+
+    BasicBlock *entryBlock = BasicBlock::Create(*TheContext, "entry", PrintFunc);
+    Builder->SetInsertPoint(entryBlock);
+    Constant* formatStr = Builder->CreateGlobalStringPtr("%d\n");
+    Builder->CreateCall(PrintfFunc, {formatStr, &*PrintFunc->arg_begin()});
+    Builder->CreateRet(ConstantInt::get(Type::getInt32Ty(*TheContext), 0, false)); // return 0
+} 
+
+int main()
+{
+    InitializeModule();
+    CreatePrintFunction();
+    std::string content = "def main()\nprint(123)";
+    MainLoop(content);
+    GenerateObject("x86_64-pc-windows");
+}
+// int token;
+// do {
+//     token = Lexer::get_token(content);
+//     std::cout << token;
+// } while (token != Lexer::tok_EOF);
